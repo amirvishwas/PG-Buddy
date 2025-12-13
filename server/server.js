8820673d844790d41a9668876b6cfbc6b1e2db08
Amir Vishwas <calvintakasi@outlook.com>
2025-12-13 16:20:36 +0530

    fix: move MongoDB connection logic to a separate connectDB.js file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/db.js";
+import connectDB from "./config/connectDB.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controller/clerkWebhooks.js";
 
