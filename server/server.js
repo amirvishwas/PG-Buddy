adb93616496dc72ef7c6c6f8b1ba453b676416e3
Amir Vishwas <calvintakasi@outlook.com>
2025-12-13 12:49:06 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controller/clerkWebhooks.js";
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+//api to listen to clerk webhooks
+app.use("/api/clerk", clerkWebhooks);
+//clerk middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/", (res, req) => req.send("Hello from pgbuddy server <3"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
