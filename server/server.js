7d2560032b7cb4f23f8da4cc0fb83056f9a2fed2
Amir Vishwas <calvintakasi@outlook.com>
2025-12-26 20:39:21 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -18,7 +18,11 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://pgbuddy1.vercel.app",
+    ],
     credentials: true,
   })
 );
